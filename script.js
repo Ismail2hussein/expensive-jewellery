cc8df9470560715c04186c756421056cc4f68f8c
Ismail2hussein <ihusseinmohamed629@gmail.com>
2023-06-09 21:23:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Good Luck 💪🏾
+function mostExpensive(jewelry) {
+  let maxPrice = 0;
+  let maxName = '';
+
+  for (let name in jewelry) {
+    if (jewelry[name] > maxPrice) {
+      maxPrice = jewelry[name];
+      maxName = name;
+    }
+  }
+
+  return `The most expensive one is the ${maxName}`;
+}
+
+console.log(mostExpensive({
+  "Silver Bracelet": 280,
+  "Gemstone Earrings": 180,
+  "Diamond Ring": 5500
+})); // "The most expensive one is the Diamond Ring"
